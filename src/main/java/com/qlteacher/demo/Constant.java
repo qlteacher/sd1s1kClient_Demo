295d8cdbab96adbf0877fab451f3f3d1837cd372
zhangpeng <zhangpengno5@163.com>
2019-04-19 18:08:07 +0800

    Update Constant.java
    增加说明

--- src/main/java/com/qlteacher/demo/Constant.java
+++ src/main/java/com/qlteacher/demo/Constant.java
@@ -2,15 +2,15 @@ package com.qlteacher.demo;
 
 public final class Constant {
 
-	public static final String clientId = "1s1k";
+	public static final String clientId = "1s1k";//写死
 
-	public static final String cientSecret = clientId;
+	public static final String cientSecret = clientId;//写死
 
-	public static final String userName = "XXXXXXXXXXX";
+	public static final String userName = "改成自己的";
 
-	public static final String password = "XXXXXXXXXXXX";
+	public static final String password = "改成自己的";
 
-	public static final String projectId = "sd2019";
+	public static final String projectId = "sd2019";//一般情况下是sd+当前年份
 	
 	public static final String partConf = "2019xxczgz";//小学初中高中用2019xxczgz,幼儿园用2019yey
 
